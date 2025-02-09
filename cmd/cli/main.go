eda89f26d2b4a567ee0d619c2ccd2e0183e404c5
andrewnguyen22 <andrew@ginchu.com>
2025-02-09 17:41:44 -0400

    added print for sleep

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -72,7 +72,9 @@ func Start() {
 	// allow sleep and wake up using config
 	wakeDate := time.Unix(int64(config.SleepUntil), 0)
 	if time.Now().Before(wakeDate) {
-		time.Sleep(time.Until(wakeDate))
+		untilTime := time.Until(wakeDate)
+		l.Infof("Sleeping until %s", untilTime.String())
+		time.Sleep(untilTime)
 	}
 	// create a new database object from the config
 	db, err := store.New(config, l)
