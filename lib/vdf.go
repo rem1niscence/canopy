ff3cb1a1df70e981eb2a0124ae127bda52c08c42
andrewnguyen22 <andrew@ginchu.com>
2025-03-07 01:59:36 -0800

    commented and audited util and vdf.go in lib

--- lib/vdf.go
+++ lib/vdf.go
@@ -11,6 +11,8 @@ const (
 	StartingIterationsPerSecond = 550
 	// IterationsFixedDecreasePercent is a 'best-guess' iteration reduction when a Stop() is called before finishing
 	IterationsFixedDecreasePercent = float64(10)
+	// EstimateIterations configures the number of iterations when starting up to 'estimate' the iterations per second
+	EstimateIterations = 1 // more iterations  = longer startup time
 )
 
 // VDFService is a structure that wraps Verifiable Delay Functionality
@@ -35,22 +37,31 @@ type VDFService struct {
 
 // NewVDFService() creates a new instance of the VDF service
 func NewVDFService(targetTime time.Duration, log LoggerI) (vdf *VDFService) {
+	// initialize the vdf service using a target time, a stop channel, and a log
 	vdf = &VDFService{TargetTime: targetTime, stopChan: make(chan struct{}, 100), running: &atomic.Bool{}, log: log}
+	// do a quick estimation of how many VDF iterations may be completed each second without delaying the app start
 	vdf.estimateIterationsPerSecond()
+	// exit
 	return
 }
 
 // Run() *blocking call*:  generates a VDF proof using the current params state of the VDF Service object
 // The design is to save the results
 func (vdf *VDFService) Run(seed []byte) {
-	vdf.log.Debugf("Starting the VDF service with %d iterations", vdf.Iterations)
+	// if the vdf service is nil
 	if vdf == nil {
+		vdf.log.Debugf("Not running VDF - empty VDF service")
+		// exit
 		return
 	}
+	// log the initialization of the vdf services
+	vdf.log.Debugf("Starting the VDF service with %d iterations", vdf.Iterations)
 	// - Run() and not running locks and starts a run
 	// - Run() and already running returns
 	if !vdf.running.CompareAndSwap(false, true) {
+		// log the active status of the VDF
 		vdf.log.Debug("VDF service is already running")
+		// exit without running again
 		return
 	}
 	// clear the results object
@@ -61,6 +72,7 @@ func (vdf *VDFService) Run(seed []byte) {
 	startTime := time.Now()
 	// ensure no 0 iteration vdf
 	if vdf.Iterations == 0 {
+		// set iterations to a target of 1
 		vdf.Iterations = 1
 	}
 	// run the VDF generation - if Stop() called, this will exit prematurely with y and proof being nil
@@ -76,6 +88,7 @@ func (vdf *VDFService) Run(seed []byte) {
 	}
 	// get the duration of the VDF run
 	duration := time.Since(startTime)
+	// log the completion of the service
 	vdf.log.Debugf("VDF service completed with %d iterations in %s", vdf.Iterations, duration.String())
 	// save the result
 	vdf.Results = crypto.VDF{
@@ -91,26 +104,34 @@ func (vdf *VDFService) Run(seed []byte) {
 // - already running signals a stop in the running thread and returns
 // - not running returns
 func (vdf *VDFService) Finish() (results *crypto.VDF) {
+	// if the VDF is empty
 	if vdf == nil {
+		// exit with nil result
 		return
 	}
+	// log the quit signal
 	vdf.log.Debugf("End signaled for VDF service")
 	// if service has not yet completed, signal to stop
 	if vdf.running.Load() {
+		// log the early stop
 		vdf.log.Warn("Prematurely stopping VDF service")
+		// signal to stop the VDF
 		vdf.stopChan <- struct{}{} // NOTE: multiple sequential calls to stop is not supported
+		// exit with nil result
 		return
 	}
 	// if output is empty, it's a premature exit
 	if vdf.Results.Output == nil {
+		// exit with nil result
 		return
 	}
-	// return last (run) iterations
+	// exit the last (run) iterations
 	return &vdf.Results
 }
 
 // VerifyVDF() verifies the VDF using the seed, the proof, and the number of iterations
 func (vdf *VDFService) VerifyVDF(seed []byte, results *crypto.VDF) bool {
+	// verify the VDF run using the crypto package
 	return crypto.VerifyVDF(seed, results.Output, results.Proof, int(results.Iterations))
 }
 
@@ -119,16 +140,20 @@ func (vdf *VDFService) VerifyVDF(seed []byte, results *crypto.VDF) bool {
 // so theoretically this a safe starting place
 func (vdf *VDFService) estimateIterationsPerSecond() {
 	var (
-		totalTime  time.Duration
-		iterations = 1 // more iterations  = longer startup time
+		// create a variable to track the total time spent
+		totalTime time.Duration
 	)
-	for i := 0; i < iterations; i++ {
+	// for each 'estimate' iteration
+	for i := 0; i < EstimateIterations; i++ {
+		// track the beginning time
 		startTime := time.Now()
+		// execute a VDF for 'starting iterations'
 		_, _ = crypto.GenerateVDF(nil, StartingIterationsPerSecond, nil)
+		// add the 'iteration time' to the 'total time'
 		totalTime += time.Since(startTime)
 	}
 	// calculate average seconds per iteration
-	averageSeconds := totalTime.Seconds() / float64(iterations)
+	averageSeconds := totalTime.Seconds() / float64(EstimateIterations)
 	// set the iterations number based on the deviation from 1 second
 	vdf.Iterations = int(float64(StartingIterationsPerSecond) / averageSeconds)
 }
@@ -140,5 +165,6 @@ func (vdf *VDFService) adjustIterations(actualTime time.Duration) {
 	adjustmentCoefficient := vdf.TargetTime.Seconds() / actualTime.Seconds()
 	// new_iterations = old_iterations * coefficient
 	vdf.Iterations = int(float64(vdf.Iterations) * adjustmentCoefficient)
+	// log the adjustment
 	vdf.log.Debugf("Adjusted iterations for next run to %d", vdf.Iterations)
 }
