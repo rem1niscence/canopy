f48385dd7fdf1d54a5c3ffae2a2dedbf738be70d
andrewnguyen22 <andrew@ginchu.com>
2025-03-13 17:19:55 -0400

    allow nextjs base path configuration

--- cmd/rpc/web/explorer/next.config.js
+++ cmd/rpc/web/explorer/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: process.env.EXPLORER_BASE_PATH || '',  // default to root if no wallet base path
 };
 
 module.exports = nextConfig;
--- cmd/rpc/web/wallet/next.config.js
+++ cmd/rpc/web/wallet/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+    output: "export",
+    basePath: process.env.WALLET_BASE_PATH || '',  // default to root if no wallet base path
 };
 
 module.exports = nextConfig;
