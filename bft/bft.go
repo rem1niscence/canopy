d458b6cc3832e39e462d86fcb9d1098103ca9a55
Pablo Ocampo <pablojocampogo@gmail.com>
2025-03-26 17:25:21 -0400

    chore: add momentary panic for easier testing

--- bft/bft.go
+++ bft/bft.go
@@ -3,11 +3,12 @@ package bft
 import (
 	"bytes"
 	"fmt"
-	"github.com/canopy-network/canopy/lib"
-	"github.com/canopy-network/canopy/lib/crypto"
 	"sort"
 	"sync/atomic"
 	"time"
+
+	"github.com/canopy-network/canopy/lib"
+	"github.com/canopy-network/canopy/lib/crypto"
 )
 
 // BFT is a structure that holds data for a Hotstuff BFT instance
@@ -178,6 +179,9 @@ func (b *BFT) HandlePhase() {
 // - Replicas run the Cumulative Distribution Function and a 'practical' Verifiable Random Function
 // - If they are a candidate they send the VRF Out to the replicas
 func (b *BFT) StartElectionPhase() {
+	if b.Round == 2 {
+		panic("TESTNET HALTED AT ROUND 2")
+	}
 	b.log.Infof(b.View.ToString())
 	// retrieve Validator object from the ValidatorSet
 	selfValidator, err := b.ValidatorSet.GetValidator(b.PublicKey)
