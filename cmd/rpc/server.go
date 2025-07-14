132284423b32678abdf849e44d91d39bdc2ef34e
andrewnguyen22 <andrew@ginchu.com>
2025-07-14 14:45:01 -0400

    increment version string

--- cmd/rpc/server.go
+++ cmd/rpc/server.go
@@ -31,7 +31,7 @@ import (
 const (
 	colon = ":"
 
-	SoftwareVersion = "0.0.0-alpha"
+	SoftwareVersion = "beta-0.1.2"
 	ContentType     = "Content-MessageType"
 	ApplicationJSON = "application/json; charset=utf-8"
 
