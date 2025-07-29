a85c0ee622a1881c0f966cc3fa88ac0ff39c3cc2
Roniel Valdez <roniel_valdez@outlook.com>
2025-07-29 21:36:38 +0200

    chore: bump version

--- cmd/rpc/server.go
+++ cmd/rpc/server.go
@@ -2,7 +2,6 @@ package rpc
 
 import (
 	"bytes"
-	"embed"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -31,7 +30,7 @@ import (
 const (
 	colon = ":"
 
-	SoftwareVersion = "beta-0.1.8"
+	SoftwareVersion = "beta-0.1.9"
 	ContentType     = "Content-MessageType"
 	ApplicationJSON = "application/json; charset=utf-8"
 
@@ -168,9 +167,9 @@ func (s *Server) updatePollResults() {
 // startStaticFileServers starts a file server for the wallet and explorer
 func (s *Server) startStaticFileServers() {
 	s.logger.Infof("Starting Web Wallet 🔑 http://localhost:%s ⬅️", s.config.WalletPort)
-	s.runStaticFileServer(walletFS, walletStaticDir, s.config.WalletPort, s.config)
+	// s.runStaticFileServer(walletFS, walletStaticDir, s.config.WalletPort, s.config)
 	s.logger.Infof("Starting Block Explorer 🔍️ http://localhost:%s ⬅️", s.config.ExplorerPort)
-	s.runStaticFileServer(explorerFS, explorerStaticDir, s.config.ExplorerPort, s.config)
+	// s.runStaticFileServer(explorerFS, explorerStaticDir, s.config.ExplorerPort, s.config)
 }
 
 // submitTx submits a transaction to the controller and writes http response
@@ -308,12 +307,6 @@ func (h logHandler) Handle(resp http.ResponseWriter, req *http.Request, p httpro
 	// Call the actual handler function with the response, request, and parameters.
 	h.h(resp, req, p)
 }
-
-//go:embed all:web/explorer/out
-var explorerFS embed.FS
-
-//go:embed all:web/wallet/out
-var walletFS embed.FS
 
 // runStaticFileServer creates a web server serving static files
 func (s *Server) runStaticFileServer(fileSys fs.FS, dir, port string, conf lib.Config) {
