4e481f69e5d0dd308e4675dcf2629085cd75e7bc
Roniel Valdez <roniel_valdez@outlook.com>
2025-09-11 19:02:08 +0200

    bump

--- cmd/rpc/server.go
+++ cmd/rpc/server.go
@@ -31,7 +31,7 @@ import (
 const (
 	colon = ":"
 
-	SoftwareVersion = "beta-0.1.10"
+	SoftwareVersion = "beta-0.1.11"
 	ContentType     = "Content-MessageType"
 	ApplicationJSON = "application/json; charset=utf-8"
 
