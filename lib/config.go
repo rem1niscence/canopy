dadef13e1203e32620a2b8b25f2200b67022ff49
Roniel Valdez <roniel_valdez@outlook.com>
2025-11-15 01:22:11 +0100

    fix: incorrect compaction calculation

--- lib/config.go
+++ lib/config.go
@@ -250,7 +250,7 @@ func DefaultStoreConfig() StoreConfig {
 		DBName:                "canopy",                       // 'canopy' database name
 		IndexByAccount:        true,                           // index transactions by account
 		InMemory:              false,                          // persist to disk, not memory
-		LSSCompactionInterval: uint64(rand.Int32N(501) + 600), // clean every 500-600 blocks (random)
+		LSSCompactionInterval: uint64(rand.Int32N(101) + 500), // clean every 500-600 blocks (random)
 	}
 }
 
