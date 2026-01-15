f3db41ce24e8bf330ac8f75331c9b3a8e786a665
Roniel Valdez <roniel_valdez@outlook.com>
2026-01-15 18:47:47 +0100

    revert: docker build

--- cmd/rpc/server.go
+++ cmd/rpc/server.go
@@ -2,6 +2,7 @@ package rpc
 
 import (
 	"bytes"
+	"embed"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -169,7 +170,9 @@ func (s *Server) updatePollResults() {
 // startStaticFileServers starts a file server for the wallet and explorer
 func (s *Server) startStaticFileServers() {
 	s.logger.Infof("Starting Web Wallet 🔑 http://localhost:%s ⬅️", s.config.WalletPort)
+	s.runStaticFileServer(walletFS, walletStaticDir, s.config.WalletPort, s.config)
 	s.logger.Infof("Starting Block Explorer 🔍️ http://localhost:%s ⬅️", s.config.ExplorerPort)
+	s.runStaticFileServer(explorerFS, explorerStaticDir, s.config.ExplorerPort, s.config)
 }
 
 // submitTx submits a transaction to the controller and writes http response
@@ -298,6 +301,12 @@ func (h logHandler) Handle(resp http.ResponseWriter, req *http.Request, p httpro
 	// Call the actual handler function with the response, request, and parameters.
 	h.h(resp, req, p)
 }
+
+//go:embed all:web/explorer/out
+var explorerFS embed.FS
+
+//go:embed all:web/wallet/out
+var walletFS embed.FS
 
 // runStaticFileServer creates a web server serving static files
 func (s *Server) runStaticFileServer(fileSys fs.FS, dir, port string, conf lib.Config) {
